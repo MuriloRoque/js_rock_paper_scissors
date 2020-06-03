f405759494c99b14ec877cf7bba67fd14852990d
Murilo Roque <muriloengqui@gmail.com>
2020-06-03 12:21:23 -0300

    add playRound function

--- scripts.js
+++ scripts.js
@@ -1,5 +1,29 @@
-function computerPlay() {
+function computerPlay(){
   arr = ['Rock', 'Paper', 'Scissors'];
   let play = arr[Math.floor(Math.random() * arr.length)];
   return play;
+}
+
+function playRound(playerSelection, computerSelection){
+  if(playerSelection == computerSelection){
+    return "Draw!"
+  }
+  else if((playerSelection == "Rock") && (computerSelection == "Scissors")){
+    return "Player won!"
+  }
+  else if((playerSelection == "Paper") && (computerSelection == "Rock")){
+    return "Player won!"
+  }
+  else if((playerSelection == "Scissors") && (computerSelection == "Paper")){
+    return "Player won!"
+  }
+  else if((playerSelection == "Paper") && (computerSelection == "Scissors")){
+    return "Computer won!"
+  }
+  else if((playerSelection == "Scissors") && (computerSelection == "Rock")){
+    return "Computer won!"
+  }
+  else if((playerSelection == "Rock") && (computerSelection == "Paper")){
+    return "Computer won!"
+  }
 }
