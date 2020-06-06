24e812bfb6a3068fcdc8aef8ac36fc2297edbcaa
Murilo Roque <muriloengqui@gmail.com>
2020-06-06 10:49:03 -0300

    implement frontend score and declaring victory

--- scripts.js
+++ scripts.js
@@ -14,47 +14,129 @@ function computerPlay(){
 
 function playRound(playerSelection, computerSelection){
   if(playerSelection == computerSelection){
-    return "Draw!"
+    return "Draw!";
   }
   else if((playerSelection == "rock") && (computerSelection == "scissors")){
-    return "Player won!"
+    return "Player won!";
   }
   else if((playerSelection == "paper") && (computerSelection == "rock")){
-    return "Player won!"
+    return "Player won!";
   }
   else if((playerSelection == "scissors") && (computerSelection == "paper")){
-    return "Player won!"
+    return "Player won!";
   }
   else if((playerSelection == "paper") && (computerSelection == "scissors")){
-    return "Computer won!"
+    return "Computer won!";
   }
   else if((playerSelection == "scissors") && (computerSelection == "rock")){
-    return "Computer won!"
+    return "Computer won!";
   }
   else if((playerSelection == "rock") && (computerSelection == "paper")){
-    return "Computer won!"
-  }
-  else{
-    return "Error"
+    return "Computer won!";
   }
 }
 
 function playGame(playerSelection){
-  let computerVar = computerPlay()
+  let computerVar = computerPlay();
   return playRound(playerSelection, computerVar);
 }
 
+const playerScore = document.querySelector('#player-score');
+const computerScore = document.querySelector('#computer-score');
+let pScore = 0;
+let cScore = 0;
+
 rock.addEventListener('click', function (e) {
-  alert(playGame("rock"));
+  let game = playGame("rock");
+  if (game == "Player won!"){
+    pScore += 1;
+  }
+  else if(game == "Computer won!"){
+    cScore += 1;
+  }
+  if(pScore == 5){
+    alert("Player won!");
+    pScore = 0;
+    cScore = 0;
+  }
+  else if(cScore == 5){
+    alert("Computer won!");
+    pScore = 0;
+    cScore = 0;
+  }
+  playerScore.textContent = pScore;
+  computerScore.textContent = cScore;
 });
 
 paper.addEventListener('click', function (e) {
-  alert(playGame("paper"));
+  let game = playGame("paper");
+  if (game == "Player won!"){
+    pScore += 1;
+  }
+  else if(game == "Computer won!"){
+    cScore += 1;
+  }
+  if(pScore == 5){
+    alert("Player won!");
+    pScore = 0;
+    cScore = 0;
+  }
+  else if(cScore == 5){
+    alert("Computer won!");
+    pScore = 0;
+    cScore = 0;
+  }
+  playerScore.textContent = pScore;
+  computerScore.textContent = cScore;
 });
 
 scissors.addEventListener('click', function (e) {
-  alert(playGame("scissors"));
+  let game = playGame("scissors");
+  if (game == "Player won!"){
+    pScore += 1;
+  }
+  else if(game == "Computer won!"){
+    cScore += 1;
+  }
+  if(pScore == 5){
+    alert("Player won!");
+    pScore = 0;
+    cScore = 0;
+  }
+  else if(cScore == 5){
+    alert("Computer won!");
+    pScore = 0;
+    cScore = 0;
+  }
+  playerScore.textContent = pScore;
+  computerScore.textContent = cScore;
 });
+
+// function score(player){
+//   let i = 0;
+//   let playerScore = 0;
+//   let computerScore = 0;
+//   let winner = "";
+//   let decision = playGame();
+//   if(decision == "Player won!"){
+//     playerScore ++;
+//   }
+//   else if(decision == "Computer won!"){
+//     computerScore ++;
+//   }
+//   i++;
+// }
+//   if(playerScore > computerScore){
+//     winner = "Player!";
+//   }
+//   else if(computerScore > playerScore){
+//     winner = "Computer!";
+//   }
+//   else{
+//     winner = "Draw!";
+//   }
+//   return "Final results: player " + playerScore + " and computer " + computerScore + ". Winner: " + winner;
+// } 
 
 // function game(){
 //   let i = 0;
